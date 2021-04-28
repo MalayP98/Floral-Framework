1fe7a79120161d679214331193a2351b77f5d467
MalayP98 <tomalaypandey.tanu@gmail.com>
2021-04-28 22:25:02 +0530

    Constants Added

---
+++ src/main/java/com/utils/Constants.java
@@ -0,0 +1,10 @@
+package com.utils;
+
+public class Constants {
+
+  public static final String STATUS = "status";
+  public static final String MESSAGE = "message";
+  public static final String TIMESTAMP = "timestamp";
+  public static final String SUCCESS = "success";
+  public static final String FAILED = "failed";
+}
