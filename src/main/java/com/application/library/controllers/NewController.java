bf0e43f87a2c2fd8727a9bd55005872f3f17accd
Malay Pandey <tomalaypandey.tanu@gmail.com>
2021-07-16 17:29:07 +0530

    working without ide

--- src/main/java/com/application/library/controllers/NewController.java
+++ src/main/java/com/application/library/controllers/NewController.java
@@ -1,18 +1,15 @@
 package com.application.library.controllers;
 
-import com.application.library.models.LoginForm;
 import com.dummyframework.annotations.Controller;
-import com.dummyframework.annotations.RequestBody;
 import com.dummyframework.annotations.RequestMapping;
 import com.dummyframework.utils.RequestMethod;
 
 @Controller
 public class NewController {
 
-  @RequestMapping(value = "/add", method = RequestMethod.POST, produces = "text/plain")
-  public String add(@RequestBody(tag = "x") LoginForm x, @RequestBody(tag = "y") int y) {
-    System.out.println(x);
-    return String.valueOf(y);
+  @RequestMapping(value = "/add", method = RequestMethod.GET, produces = "text/plain")
+  public String add() {
+    return "Hello World!";
   }
 
 }
